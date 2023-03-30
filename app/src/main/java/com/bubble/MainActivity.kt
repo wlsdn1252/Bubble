595d4934f0f6c57c553aad5dc216be0b24dfd02b
admin <wlsdn1252@naver.com>
2023-03-30 15:15:05 +0900

    첫 로딩시 앱 로고화면 4초동안 띄우고 화면 전환

--- app/src/main/java/com/bubble/MainActivity.kt
+++ app/src/main/java/com/bubble/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.bubble
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
